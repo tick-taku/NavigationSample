1772a74325d3f8653454be38267cc45d107b4bec
Takumi Ito <ito.takumi@ditgroup.jp>
2019-09-23 22:37:02 +0900

    Add navigation extension function of googleexample.

--- app/src/main/java/com/jetpack/navigation/MainActivity.kt
+++ app/src/main/java/com/jetpack/navigation/MainActivity.kt
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.observe
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.jetpack.navigation.databinding.ActivityMainBinding
 import com.jetpack.navigation.util.dataBinding
+import com.jetpack.navigation.util.setupWithNavController
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
@@ -38,9 +41,6 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private fun setupToolbar() {
         setSupportActionBar(binding.toolbar)
 
-        setupActionBarWithNavController(findNavController(R.id.nav_host_fragment),
-                                        AppBarConfiguration(topLevelScreen))
-
         if (Build.VERSION_CODES.M <= Build.VERSION.SDK_INT) {
             binding.root.apply {
                 systemUiVisibility = systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
@@ -53,8 +53,15 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
      * Setup for BottomNavigationView
      */
     private fun setupBottomNavigation() {
-        val navController = findNavController(R.id.nav_host_fragment)
-        binding.bottomNavigation.setupWithNavController(navController)
+        val navGraph = listOf(R.navigation.number_graph,
+                              R.navigation.bike_graph,
+                              R.navigation.star_graph)
+        binding.bottomNavigation
+            .setupWithNavController(navGraph, supportFragmentManager, R.id.nav_host_fragment, intent)
+            .observe(this) {
+                // Bind toolbar for navigation
+                setupActionBarWithNavController(it, AppBarConfiguration(topLevelScreen))
+            }
     }
 
     override fun onSupportNavigateUp() = findNavController(R.id.nav_host_fragment).navigateUp()
